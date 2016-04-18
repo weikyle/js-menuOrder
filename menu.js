1f2b2c038de18be0f8ecdcad8e6c0bad8f630442
Kyle Wei <weikyle@wustl.edu>
2016-04-18 18:30:19 -0500

    menu

--- menu.js
+++ menu.js
@@ -1,15 +1,56 @@
 // begin with a blank/new order
-var order = '';
+var type = '';
+var tortilla = '';
+var meat = '';
+var rice = '';
+var beans = '';
+var toppings = '';
 
 
 // build a function to ask questions
 
-function question() {
+function question(n, q) {
   // your code goes here
+  if ( n === 1){
+  	type = prompt(q);
+  	if (type.toUpperCase() === 'taco'){
+  		tortilla = prompt("Tortilla (soft flour, soft corn, or crispy corn): ");
+  	}
+  }
+  else if ( n === 2){
+  	meat = prompt(q);
+  	
+  }
+  else if ( n === 3){
+  	rice = prompt(q);
+  	
+  }
+  else if ( n === 4){
+  	beans = prompt(q);
+  	
+  }
+  else if ( n === 5){
+  	toppings = prompt(q);
+  	
+  }
+
+
 }
 
 // call the QUESTION function with the order options
-question();
+question(1,"Burrito, bowl, taco, or salad: ");
+question(2,"Meat (chicken, steak, barbacoa, carnitas, sofritas, or veggie): ");
+question(3,"Rice (none, brown, white): ");
+question(4,"Beans (none, black, pinto): ");
+question(5, "Toppings (fajita veggies, tomato salsa, green chili salsa, red chili salsa, sour cream, cheese, guacamole, lettuce): ");
+
+
+
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+document.write("<p>Your order: " + "<p>");
+document.write("<p>Type: " + tortilla + type + "<p>");
+document.write("<p>Meat: " + meat + "<p>");
+document.write("<p>Rice: " + rice + "<p>");
+document.write("<p>Beans: " + beans + "<p>");
+document.write("<p>Toppings: " + toppings + "<p>");
