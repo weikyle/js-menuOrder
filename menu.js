fd0ea1915f66099466139c29512a3dbeb7ebe396
Kyle Wei <weikyle@wustl.edu>
2016-04-18 18:35:28 -0500

    menu

--- menu.js
+++ menu.js
@@ -5,6 +5,7 @@ var meat = '';
 var rice = '';
 var beans = '';
 var toppings = '';
+var order = '';
 
 
 // build a function to ask questions
@@ -13,24 +14,29 @@ function question(n, q) {
   // your code goes here
   if ( n === 1){
   	type = prompt(q);
-  	if (type.toUpperCase() === 'taco'){
+  	if (type.toUpperCase() === 'TACO'){
   		tortilla = prompt("Tortilla (soft flour, soft corn, or crispy corn): ");
+  		order += tortilla + " " + type + " with ";
   	}
   }
   else if ( n === 2){
   	meat = prompt(q);
+  	order += meat + ", ";
   	
   }
   else if ( n === 3){
   	rice = prompt(q);
+  	order += rice + " rice, ";
   	
   }
   else if ( n === 4){
   	beans = prompt(q);
+  	order += beans + " beans, and ";
   	
   }
   else if ( n === 5){
   	toppings = prompt(q);
+  	order += toppings;
   	
   }
 
@@ -48,8 +54,8 @@ question(5, "Toppings (fajita veggies, tomato salsa, green chili salsa, red chil
 
 
 // output their order
-document.write("<p>Your order: " + "<p>");
-document.write("<p>Type: " + tortilla + type + "<p>");
+document.write("<p>Your order: " + order + "<p>");
+document.write("<p>Type: " + tortilla + " " + type + "<p>");
 document.write("<p>Meat: " + meat + "<p>");
 document.write("<p>Rice: " + rice + "<p>");
 document.write("<p>Beans: " + beans + "<p>");
